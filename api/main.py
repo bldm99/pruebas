294eb2aafd674035e6373080cd51395d8ed4bd1d
bldm99 <bladimir.chipana@tecsup.edu.pe>
2023-12-13 13:33:59 -0500

    er

---
+++ api/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import os
+import json
+import redis
+from flask import Flask, request, jsonify
+from flask_cors import CORS , cross_origin
+
+
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+redis_conn = redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379"))
+
+
+
+
+@app.route("/")
+def index():
+    return "Usage: http://<hostname>[:<prt>]/api/<url>"
+
+
+app.run(host="0.0.0.0")
